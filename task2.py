0b563469cd47744b7ac56f329bd3e5206662dfa0
KDThaGoatt <kyand906@deltalearns.ca>
2025-05-20 10:55:43 -0700

    a

--- task2.py
+++ task2.py
@@ -32,11 +32,31 @@ appropriate use of return values and input parameters
 # a variable. We will use it as the basis for this 
 # assignment.
 
-while True:
-    y = keyboard.read_key()
-    print(y)
-    print(time.time())
-    t = time.localtime()
-    print(t)
-    print(time.strftime("%H",t))
-    break
+alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+
+def character():
+    char = alphabet[random.randint(0,25)]
+    return char
+
+def run():
+    print("type all characters")
+    x = time.time()
+    for i in range(10):
+        char = character()
+        print(char)
+        y = keyboard.read_key()
+        print(y)
+        if y != char:
+            print("wrong input!")
+            i = i-1
+            break
+        else:
+            print("correct!")
+        time.sleep(0.1)
+    
+    now = time.time()
+    elapsed = now - x
+    rounded = round(elapsed, 2)
+    print(f"You typed {i+1} characters in {rounded} seconds!")
+
+run()
